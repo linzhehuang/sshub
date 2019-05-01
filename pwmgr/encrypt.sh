3f1dc00118e367283f661f8473f7ba5f01e0a135
linzhehuang <linzhehuang@outlook.com>
2019-05-01 16:02:10 +0800

    optimize encrypt.sh
    * optimize processing speed of encrypt.sh by using awk
    * replace new encrypt.sh in pwmgr

--- pwmgr/encrypt.sh
+++ pwmgr/encrypt.sh
@@ -3,13 +3,9 @@
 # encrypt(input, output, password)
 encrypt() {
   # read_file(file)
-  # usage: eval data=$( read_file "file")
   read_file() {
-    local _format='16/1 "%02x" " "'
-    local data=$( hexdump -v -e "${_format}" "${1}" )
-    local data_size=${#data}
-    datasize=$(( data_size - 1 ))
-    echo -n "(${data:0:${data_size}})"
+    local data=$( hexdump -v -e '1/1 "%02x" " "' "${1}" )
+    echo -ne "${data:0:$(( ${#data} - 1 ))}"
   }
 
   # get_md5(value)
@@ -18,22 +14,54 @@ encrypt() {
     echo -n "${ret%  -*}"
   }
 
-  local data md5 block block_size out_data
-  local i l r
+  local data md5 out_data
   md5=$( get_md5 "${3}" )
   out_data=""
 
-  eval data=$( read_file "${1}") # read file as array
-  for block in ${data[@]}; do
-    block_size=${#block}
-    for i in $( seq 0 2 $(( block_size - 1 )) ); do
-      l="${block:${i}:2}"
-      r="${md5:${i}:2}"
-      l=$(( 16#${l} ^ 16#${r} ))
+  data=$( read_file "${1}" ) # read file
+  out_data=$( echo -ne "${data}"|awk -v "md5=${md5}" '
+  BEGIN {
+    # preprocess md5
+    for (i = 1; i < 32; i += 2) {
+      num = "0x"substr(md5, i, 2);
+      arr[int(i/2)+1] = num + 0;
+    }
+  }
+  {
+    data_size = split($0, data, " ");
+    for (i = 1; i <= data_size; i++) {
+      r = "0x"data[i]; r = r + 0;
+      l = arr[(i-1)%16 + 1];
 
-      out_data="${out_data}$( printf '\\x%02x' "${l}" )"
-    done
-  done
+      #printf("%d %d\n", r, l);
+
+      #ret = r xor l
+      ret = 0;
+  		bit = 0;
+      while (l != 0 && r != 0) {
+        t = ((l % 2) == (r % 2)) ? 0 : 1;
+        ret = ret + (2^bit)*t;
+        l = int(l / 2);
+        r = int(r / 2);
+        bit++;
+      }
+      while (l != 0) {
+        t = ((l % 2) == 0) ? 0 : 1;
+        ret = ret + (2^bit)*t;
+        l = int(l / 2);
+        bit++;
+      }
+      while (r != 0) {
+        t = ((r % 2) == 0) ? 0 : 1;
+        ret = ret + (2^bit)*t;
+        r = int(r / 2);
+        bit++;
+      }
+      printf("\\x%02x", ret);
+
+    }
+  }
+  ' )
 
   # write data to file
   echo -ne "${out_data}" > "${2}"
